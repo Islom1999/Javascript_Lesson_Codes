9424f4781427036f119465202b6b2919468582fa
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-01 22:05:47 +0500

    Javascript lesson one and two loaded

---
+++ 28-dars/js/script.js
@@ -0,0 +1,23 @@
+/*let num1 = 5;
+let num2 = 15;
+
+
+let num3 = num1 + num2;
+let num4 = num1 - num2;
+let num5 = num1 * num2;
+let num6 = num1 / num2;
+let num7 = num1 ** num2;
+let num8 = num1 % num2;
+
+
+ 
+console.log('num1:' ,num1);
+console.log('num2:' ,num2);
+console.log('num1 + num2 = ', num3) */
+
+let ism = prompt('Ismizni kiriting');
+let familiya = prompt('Familiyangizni kiriting');
+
+console.log("siznini to'liq ism familiyangiz ", ism+' ' +familiya);
+
+
---
+++ 29-dars/js/js.js
@@ -0,0 +1,46 @@
+
+/*let parol = 'name'
+let a = prompt('parolni kiriting')
+
+if(a == parol){
+    console.log('togri')
+}else{
+    console.log('notogri')
+}
+*/
+
+// son 2 dan kichig, 2dan katta 5dan kichik, 5 dan katta
+/*
+let bool = false && true   //va     -  mantiqiy kopaytirish
+let bool1 = false || true  //yoki   -  mantiqiy qo'shish 
+let bool2 = !true          //inkor true ni false ga falseni true ge
+*/
+
+/*a = 4
+
+if (a < 2 ){
+    console.log('yetmaydi')
+}else if(a > 2 && a < 5){
+    console.log('yetadi')
+}else{
+    console.log('yanana nimadurga pul yetadi')
+}*/
+/*
+let son = prompt('sonni kiriting')
+
+if(son < 1000 && son>=100){
+    let a = son % 10
+    let b = Math.floor(son / 100)
+    if(a == b){
+        console.log('togri')
+    }else{
+        console.log('notogri')
+    }
+    
+}else{
+    console.log('berilgan son notogri')
+}*/
+
+
+
+
