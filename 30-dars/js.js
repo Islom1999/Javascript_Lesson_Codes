0107d93b544e0ae44c14b77fbe8f45e007472d74
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-02 18:01:20 +0500

    lesson 30 loaded

---
+++ 30-dars/js.js
@@ -0,0 +1,66 @@
+
+/*let name = 45.4
+let num = Math.round(name)
+console.log(num)*/
+
+/*let name = 45.9
+let num = Math.floor(name)
+console.log(num)*/
+
+/*let name = 45.01
+let num = Math.ceil(name)
+console.log(num)*/
+/*
+console.log(Math.min(1,2,3,-9,15))
+console.log(Math.max(1,2,3,-9,15))*/
+
+/*
+let a = Math.floor(Math.random()*100) % 9
+console.log(a) */
+/*
+let a = 100
+console.log(Math.abs(a))
+console.log(Math.sqrt(9))*/
+
+/*console.log(Math.pow(1000, 1/3))*/
+/*
+prompt('required text', 'optional text')
+let number = prompt('Enter number', 'number goes here')
+console.log(number)*/
+/*
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) */
+
+/*
+let fullName = 'Islom \nKarimov'
+console.log(fullName)
+
+fullName = 'Islom \t Karimov'
+console.log(fullName)
+
+fullName = 'O\'zbekiston'
+console.log(fullName)
+*/
+/*
+let name ='Islom'
+let sureName = 'Karimov'
+console.log(name+' '+sureName)*/
+
+/*
+let name = prompt('Ismizni kiriting : ')
+let sureName = prompt('Ismizni kiriting : ')
+
+console.log(`sizning ismingiz: ${name} familiyangiz:  ${sureName}`)
+*/
+/*
+let name = 'LOREM'
+name = name.toLowerCase()
+console.log(name)*/
+
+/*
+let name = 'Anor'
+console.log(name.length)
+console.log(name[0])
+*/
+
+/*(3>5) ? console.log(true) : console.log(false)*/
