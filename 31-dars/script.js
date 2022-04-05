55296483435da76117db00fc028b281c81e20424
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-05 18:01:57 +0500

    lesson 31 loaded

---
+++ 31-dars/script.js
@@ -0,0 +1,160 @@
+/*
+let u = -50
+
+u++  //  u=u+1
+++u  //  u=u+1
+u--  //  u=u-1
+--u  //  u=u-1
+
+u+=5  // u= u+5
+u-=5  // u= u-5
+u/=5  // u= u/5
+u*=5  // u= u*5
+
+*/
+/*
+let a = 10
+console.log(a++)
+console.log(a)*/
+
+/*
+let a = '45.2'
+//a = parseInt(a)
+//a = parseFloat(a)
+//a = Number(a)
+// a = +a
+
+console.log(typeof a, a)
+*/
+
+/*
+for(let i = 1; i<=50; i++){
+    if(i%3 == 1){
+        console.log(i)
+    }
+}
+*/
+
+/*
+0 * 0 = 0
+1 * 1 = 1
+2 * 2 = 4
+3 * 3 = 9
+4 * 4 = 16
+5 * 5 = 25
+*/
+
+/*
+for(let i = 0; i<=5; i++){
+    console.log(`${i} * ${i} = ${i**2}`)
+}
+*/
+
+/*
+let arr = [1, 2, 3, false, 'fsfs']
+console.log(arr)
+
+arr[0] = 5
+
+console.log(arr)
+
+*/
+/*
+let a = 1
+let b = 2
+let c = 3
+
+let arr = [a,b,c]
+console.log(arr)*/
+
+/*
+let arr1 = [1,2,3]
+let arr = [4,5,6]
+
+let arr3 = arr1.concat(arr);
+
+console.log(arr1.concat(arr))
+console.log(arr.concat(arr1))
+console.log(arr1, arr)
+
+*/
+
+/*
+let arr = [1,2,3,4,5,6]
+
+for(let i = 0; i <= arr.length - 1; i++){
+    console.log(arr[i])
+}
+*/
+
+/*
+let arr = [1,2,3,4,5,6, 'name']
+console.log(arr.indexOf(5))
+console.log(arr.indexOf('name'))
+console.log(arr.indexOf(1))
+console.log(arr.indexOf(10))
+*/
+
+/*
+let arr = [1,2,3,4,5,6, 'name']
+console.log(arr)
+
+let index = arr.indexOf('name')
+
+if(index != -1){
+    arr[index] = 'Shaxzoda'
+}
+console.log(arr)*/
+
+/*
+let arr = [1,2,3,4,5,6, 'name']
+
+console.log(arr.includes(5))
+console.log(arr.includes('name'))
+console.log(arr.includes(1))
+console.log(arr.includes(10))
+*/
+
+/*
+let arr = [1,2,3,4,5,6, 'name']
+console.log(Array.isArray(arr))
+*/
+/*
+let arr = ['f', 'r', 'o','n', 't']
+console.log(arr.toString())*/
+
+/*
+let arr = ['f', 'r', 'o','n', 't']
+console.log(arr.join(''))
+*/
+
+/*
+let n = 10;
+let s=0
+
+for(let i = 1; i<=n; i++){
+    s+= i
+}
+console.log(s)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
