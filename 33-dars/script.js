90f1609072c469294616fa8d6bc45ed3615fd26a
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-09 17:54:33 +0500

    lesson 33 loaded

---
+++ 33-dars/script.js
@@ -0,0 +1,286 @@
+
+
+/*
+let a = +prompt('1-sonni son kititing: ')
+let b = +prompt('2-sonni son kititing: ')
+let belgi = prompt('Birovini tanlang: "+", \'-\', \'*\', "/"')
+
+switch (belgi){
+    case '+':
+        console.log(a+'+'+b+'='+(a+b)); break;
+    case '-':
+        console.log(a+'-'+b+'='+(a-b)); break
+    case '*':
+        console.log(a+'*'+b+'='+(a*b)); break;
+    case '/':
+        console.log(a+'/'+b+'='+(a/b)); break;
+}
+*/
+
+/*
+for(let i=1; i<=10; i++){
+    console.log('hello !')
+    continue
+    console.log('hi !')
+}
+for(let i=1; i<=10; i++){
+    console.log('hello !')
+    break
+    console.log('hi !')
+}
+*/
+
+/*
+let i = 0;
+while(i<=10){
+    console.log(i);
+    i+=2;
+}*/
+
+
+/*
+let s = 1024
+let n = 1
+while(2**n == s){
+    n++
+}
+console.log(n)
+
+let a = 1024
+let i = 1
+do{i++}while(2**i == a)
+console.log(i)
+*/
+
+/*
+let a=5
+while(a>0){
+    a=prompt('a ga manfiy son kiriting')
+}*/
+
+
+/*
+let a 
+do{
+    a=prompt('a ga manfiy son kiriting')
+}while(a>0)*/
+
+
+const countries = [
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Argentina',
+    'Armenia',
+    'Australia',
+    'Austria',
+    'Azerbaijan',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Belarus',
+    'Belgium',
+    'Belize',
+    'Benin',
+    'Bhutan',
+    'Bolivia',
+    'Bosnia and Herzegovina',
+    'Botswana',
+    'Brazil',
+    'Brunei',
+    'Bulgaria',
+    'Burkina Faso',
+    'Burundi',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cape Verde',
+    'Central African Republic',
+    'Chad',
+    'Chile',
+    'China',
+    'Colombi',
+    'Comoros',
+    'Congo (Brazzaville)',
+    'Congo',
+    'Costa Rica',
+    "Cote d'Ivoire",
+    'Croatia',
+    'Cuba',
+    'Cyprus',
+    'Czech Republic',
+    'Denmark',
+    'Djibouti',
+    'Dominica',
+    'Dominican Republic',
+    'East Timor (Timor Timur)',
+    'Ecuador',
+    'Egypt',
+    'El Salvador',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Estonia',
+    'Ethiopia',
+    'Fiji',
+    'Finland',
+    'France',
+    'Gabon',
+    'Gambia, The',
+    'Georgia',
+    'Germany',
+    'Ghana',
+    'Greece',
+    'Grenada',
+    'Guatemala',
+    'Guinea',
+    'Guinea-Bissau',
+    'Guyana',
+    'Haiti',
+    'Honduras',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Iran',
+    'Iraq',
+    'Ireland',
+    'Israel',
+    'Italy',
+    'Jamaica',
+    'Japan',
+    'Jordan',
+    'Kazakhstan',
+    'Kenya',
+    'Kiribati',
+    'Korea, North',
+    'Korea, South',
+    'Kuwait',
+    'Kyrgyzstan',
+    'Laos',
+    'Latvia',
+    'Lebanon',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Macedonia',
+    'Madagascar',
+    'Malawi',
+    'Malaysia',
+    'Maldives',
+    'Mali',
+    'Malta',
+    'Marshall Islands',
+    'Mauritania',
+    'Mauritius',
+    'Mexico',
+    'Micronesia',
+    'Moldova',
+    'Monaco',
+    'Mongolia',
+    'Morocco',
+    'Mozambique',
+    'Myanmar',
+    'Namibia',
+    'Nauru',
+    'Nepal',
+    'Netherlands',
+    'New Zealand',
+    'Nicaragua',
+    'Niger',
+    'Nigeria',
+    'Norway',
+    'Oman',
+    'Pakistan',
+    'Palau',
+    'Panama',
+    'Papua New Guinea',
+    'Paraguay',
+    'Peru',
+    'Philippines',
+    'Poland',
+    'Portugal',
+    'Qatar',
+    'Romania',
+    'Russia',
+    'Rwanda',
+    'Saint Kitts and Nevis',
+    'Saint Lucia',
+    'Saint Vincent',
+    'Samoa',
+    'San Marino',
+    'Sao Tome and Principe',
+    'Saudi Arabia',
+    'Senegal',
+    'Serbia and Montenegro',
+    'Seychelles',
+    'Sierra Leone',
+    'Singapore',
+    'Slovakia',
+    'Slovenia',
+    'Solomon Islands',
+    'Somalia',
+    'South Africa',
+    'Spain',
+    'Sri Lanka',
+    'Sudan',
+    'Suriname',
+    'Swaziland',
+    'Sweden',
+    'Switzerland',
+    'Syria',
+    'Taiwan',
+    'Tajikistan',
+    'Tanzania',
+    'Thailand',
+    'Togo',
+    'Tonga',
+    'Trinidad and Tobago',
+    'Tunisia',
+    'Turkey',
+    'Turkmenistan',
+    'Tuvalu',
+    'Uganda',
+    'Ukraine',
+    'United Arab Emirates',
+    'United Kingdom',
+    'United States',
+    'Uruguay',
+    'Uzbekistan',
+    'Vanuatu',
+    'Vatican City',
+    'Venezuela',
+    'Vietnam',
+    'Yemen',
+    'Zambia',
+    'Zimbabwe'
+  ]
+
+/*
+for(item of countries){
+    console.log(item)
+}
+*/
+/*
+for(let item=0; item<=countries.length-1; item++){
+    console.log(countries[item])
+}*/
+/*
+arr 
+
+arr4 
+arr5 
+arr6 
+
+for(item of countries){
+    if(item.length==4){
+        console.log(item)
+    }
+}
+*/
