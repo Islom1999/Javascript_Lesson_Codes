ee48a74a64525c694782feeb54c7f647e270ff7f
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-16 18:06:32 +0500

    36 lesson loaded

---
+++ 36-dars/script.js
@@ -0,0 +1,105 @@
+/*  decloration
+function name(){
+    console.log('Salom')
+}
+name()
+*/
+
+/*
+function name(text){
+    console.log(text)
+}
+name(452654)
+name('Salom Islom')
+name(true)
+name('Hello Wold')
+*/
+/*
+function name(text){
+    console.log(text)
+}
+name('Akobir', 'Shaxzoda', 'Ruxshona', "Go'zal")
+*/
+/*
+function name(...text){
+    console.log(...text)
+}
+name('Akobir', 'Shaxzoda', 'Ruxshona', "Go'zal")*/
+
+/*
+function name(...text){
+    console.log(text)
+}
+name('Akobir', 'Shaxzoda', 'Ruxshona', "Go'zal")*/
+
+/*
+function name(a){
+    return a*a
+}
+console.log(name(5))
+*/
+
+/*
+function name(a){
+    console.log(a**a)
+
+    return a*a
+}
+console.log(name(5))*/
+
+// function expression	
+/*
+let name = function(){
+    console.log('salom')
+}
+name()
+name = function(){
+    console.log('Hello')
+}
+name()*/
+
+/*
+let name = function(...text){
+    console.log(...text)
+}
+name(1,2,3,4,5,6,7)
+*/
+
+/*
+let name = function(...text){
+    console.log(text)
+}
+name(1,2,3,4,5,6,7)
+*/
+
+/*
+let name = function(...text){
+    let s = 0
+    for(item of text){
+        s+=item
+    }
+    return s
+}
+let yigindi = name(1,2,3,4,5,6,7)
+console.log(yigindi)
+*/
+
+
+// arrow function
+
+/*
+let name = (a) => {
+    console.log('hello')
+    return a
+}
+console.log(name('Ruxshona'))*/
+
+/*
+let name = a => {
+    console.log('hello')
+    return a
+}
+console.log(name('Ruxshona'))*/
+
+name(5,6,'+')
+
