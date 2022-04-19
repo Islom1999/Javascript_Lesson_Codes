b9b3e8e9a1c7db776ad5ca65dbc8b3b538fef1a6
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-19 18:08:09 +0500

    lesson 37 loaded

---
+++ 37-dars/script.js
@@ -0,0 +1,114 @@
+/*
+function Islom(){
+    return 'Islom'
+}
+function Akobir(){
+    return 'Akobir'
+}
+function Shaxzoda(){
+    return 'Shaxzoda'
+}
+
+function hello(func){
+    console.log('Hello ' + func())
+}
+hello(Shaxzoda)
+*/
+
+/*
+function playFootball(player, callback) {
+    console.log(`Name of player ${player}`);
+    callback();
+}
+playFootball("Messi", function() {
+    console.log("His shot 100 goals !")})
+    */
+
+    /*
+function qosh(a,b){
+    return a+b
+}
+function kan(a,b,c,func1){
+    switch(c){
+        case '+':
+            console.log(func1(a,b))
+    }
+}
+kan(1,15,'+',qosh)*/
+
+/*
+str = 'Salom'            // global
+function name (text){
+    str = 'Hi !!!'       // local
+    let arr = [1,2,3]    // local
+    console.log(str)
+}
+name("Hello")
+console.log(arr)
+console.log(str) 
+*/
+
+/*
+const a = [5]
+console.log(++a[0])*/
+
+/*
+const a = [5]
+a.push(6)
+console.log(a)
+*/
+
+/*
+const a = [5]
+a.push(6)
+a='salom'
+console.log(a)
+*/
+/*
+var a = 5;
+console.log(a)
+
+let b = 5;
+console.log(b)
+*/
+
+/*
+console.log(a)
+var a = 5;
+
+console.log(b)
+let b = 5;
+*/
+
+
+/*
+let now = new Date()
+console.log(now)
+console.log(now.getFullYear())
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getHours())
+console.log(now.getMilliseconds())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+console.log(now.getMonth())
+console.log(now.getTime())
+
+now.setFullYear(2015)
+now.setHours(15)
+now.setMinutes(15)
+now.setMonth(0)
+now.setSeconds(15)
+now.setDate(15)
+
+
+console.log(now)
+*/
+
+/*
+let tugulgan = '2019-03-22'
+let tugulganDate = new Date(tugulgan)
+let now = new Date()
+console.log(tugulganDate)
+console.log(now)
+console.log(now - tugulganDate)*/
