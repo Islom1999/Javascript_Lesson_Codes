6dd38e376ca05afecc06cd6ec8987d4cf59f894f
Karimov Islom <islomkarimovalisherovich@gmail.com>
2022-04-28 18:02:09 +0500

    lesson 41 loaded

---
+++ 41-dars/script.js
@@ -0,0 +1,117 @@
+
+/*
+const languages = [
+    'English',
+    'Finnish',
+    'English',
+    'French',
+    'Spanish',
+    'English',
+    'French',
+]
+console.log(languages)
+
+const setLang = new Set(languages)
+console.log(setLang)
+
+/*
+const obj ={
+    name: "Islom",
+    lastName: 'Karimov'
+}
+console.log(obj.name)
+*/
+/*
+setLang.add('uz')
+setLang.delete('French')
+
+
+for(item of setLang){
+    console.log(item)
+}
+console.log(setLang.size)
+console.log(setLang.has('uz'))
+
+
+setLang.clear()
+*/
+/*
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+console.log([arr1,arr2])
+console.log([...arr1,...arr2])
+*/
+
+
+/*
+countries = [
+  ['Finland', 'Helsinki'],
+  ['Sweden', 'Stockholm'],
+  ['Norway', 'Oslo'],
+]
+const map = new Map(countries)
+
+map.set("name", "Islom")
+
+console.log(map.get('name'))
+console.log(map.has('name'))
+console.log(map.has('surName'))
+
+
+console.log(map)
+console.log(map.size)
+*/
+/*
+let arr1 = ["Islom", "KARIMOV", 23]
+
+let name = arr1[0]
+let surmane = arr1[1]
+let age = arr1[2]
+
+console.log(name,surmane,age)
+*/
+/*
+let arr1 = ["Islom", "KARIMOV", 23]
+let [name, surmane, age] = arr1
+
+console.log(name,surmane,age)
+console.log(typeof name)
+console.log(typeof age)
+*/
+
+
+/*
+const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+  ]
+  const [frontEnd, backEnd] = fullStack
+  
+  console.log(frontEnd)
+  console.log(backEnd)
+  */
+
+  /*
+const countries = [['Finland', 'Helsinki'], ['Sweden', 'Stockholm'], ['Norway', 'Oslo']]
+
+for (const [country, city] of countries) {
+console.log(country, city)
+}
+*/
+
+/*
+const rectangle = {
+    width: 20,
+    height: 10,
+    area: 200
+  }
+  let { width, height, area, perimeter } = rectangle
+  
+  console.log(width, height, area, perimeter)
+  width = 50
+  console.log(typeof width, width)
+
+*/
+
+
+
