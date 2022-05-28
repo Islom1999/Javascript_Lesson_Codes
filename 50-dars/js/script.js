4bcec5c684bb8868206d06a92dc33eaeb27588b2
tizer7417 <tizer7417@gmail.com>
2022-05-28 17:46:23 +0500

    losson 50 loaded

---
+++ 50-dars/js/script.js
@@ -0,0 +1,32 @@
+let btnModal = document.querySelector('.btn-modal')
+
+let modal = document.querySelector('.modal')
+
+let modalBg = document.querySelector('.modal-bg')
+
+let isOpen = false
+
+function modalToggle(){
+    modal.style.transform = 'translate(-50%, -50%) scale(1)'
+    modalBg.style.display = 'block'
+    isOpen = true
+}
+
+btnModal.addEventListener('click', modalToggle)
+
+document.querySelector('#close-modal').addEventListener('click', () => {
+    modal.style.transform = 'translate(-50%, -50%) scale(0)'
+    modalBg.style.display = 'none'
+    isOpen = false
+})
+
+
+document.querySelector('.btn-res').addEventListener('click', () => {
+    document.querySelector('.nav-responsive').style.right = '0px'
+    
+})
+
+document.querySelector('.btn-res-close').addEventListener('click', () => {
+    document.querySelector('.nav-responsive').style.right = '-100%'
+    
+})
